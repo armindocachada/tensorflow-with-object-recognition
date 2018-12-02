4543ad0427d4ce00d010420d346e01460ade90d6
Armindo Cachada <armindo@techbiosis.com>
2018-12-02 20:20:21 +0000

    Fixed docker file

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+cd /tensorflow/models/research && export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim:`pwd`/object_detection
+export OBJECT_DETECTION_API_PATH=/tensorflow/models/research/object_detection
 cd /intruder-detector && nohup python3 security_camera.py > /data/intruder_detection_service.out 2>&1 </dev/null &
 tail -f /data/intruder_detection_service.out
