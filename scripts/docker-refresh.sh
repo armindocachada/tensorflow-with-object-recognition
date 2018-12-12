2e28ce8e62a0c777934acfa02c436d0055936a35
Armindo Cachada <armindo@techbiosis.com>
2018-12-12 16:22:42 +0000

    fixed docker-refresh

--- scripts/docker-refresh.sh
+++ scripts/docker-refresh.sh
@@ -6,7 +6,7 @@ mkdir -p ~/camera_latest
 rm -fr ~/camera_latest/*
 cd ~/camera_latest && git clone https://github.com/armindocachada/tensorflow-with-object-recognition
 rm -fr ~/camera_latest/tensorflow-with-object-recognition/credentials
-cp ~/project/credentials ~/camera_latest/tensorflow-with-object-recognition/credentials
+cp -r ~/project/credentials ~/camera_latest/tensorflow-with-object-recognition/credentials
 cd ~/camera_latest//tensorflow-with-object-recognition
 docker build -t armindocachada/tensorflow-with-object-recognition -f Dockerfile_pi .
 docker run -i -t -d -p 8888:8888 -v /share:/data/videos/incoming armindocachada/tensorflow-with-object-recognition
